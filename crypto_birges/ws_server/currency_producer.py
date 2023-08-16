bf02a09e1fc53a507d942953e2c35ce63cd1b60a
= <=>
2023-08-16 13:45:58 +0300

    Переписали на один веб-сокет. Добавили логгирование. Не работает...

--- crypto_birges/ws_server/currency_producer.py
+++ crypto_birges/ws_server/currency_producer.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import json
 import time
-
+from uuid import uuid4
 
 import websockets
 
@@ -12,7 +12,12 @@ from kucoin_client import prevent_request, subscribe, get_ping_msg, make_topics
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
+handler = logging.StreamHandler()
+
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 # данные для подключения
 creds = prevent_request()
 
@@ -31,22 +36,23 @@ topics =  make_topics()
 
 async def main():
     uri = "ws://localhost:2010"
+    connect_id = str(uuid4()).replace('-', '')
+    last_ping = time.time()
     async with websockets.connect(uri) as websocket:
         try:
-            async with websockets.connect(f"{endpoint}/?token={token}") as ws:
-                """подключаемся к 7 веб-сокетам и подписываемся на рассылку всех валют"""
-                subscribe_msg = [subscribe(topics[conn]) for conn in range(len(topics))]
-                tasks = [subscribe_channel(ws, message) for message in subscribe_msg]
-                await asyncio.gather(*tasks)
+            async with websockets.connect(f"{endpoint}/?token={token}&connectId={connect_id}") as ws:
+                """подключаемся к 1 веб-сокету и подписываемся на рассылку всех валют"""
+                subs_msg = subscribe("all")
+                await ws.send(subs_msg)
                 msg = await ws.recv()
                 channel_id = json.loads(str(msg)).get('id')
                 if channel_id:
                     ping_msg = get_ping_msg(channel_id)
-                now = time.time()
                 while True:
                     async for msg in ws:
-                        if time.time() - now >= ping_timeout + ping_interval:
-                            await ws.send(ping_msg)
+                        if time.time() - last_ping >= ping_interval:
+                            await ws.send(ping_msg(str(int(time.time()) * 1000)), timeout=ping_timeout)
+                            await websocket.ping()
                         now = time.time()
                         await websocket.send(json.dumps(msg))
         except websockets.exceptions.ConnectionClosedOK:
